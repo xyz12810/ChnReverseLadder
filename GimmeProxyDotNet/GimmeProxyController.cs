bfd95d6e19717fbc7da3507b9d1cc46f990fa767
Jackson Ming Hu <huming2207@gmail.com>
2016-08-14 22:58:44 +1000

    Initial import for GimmeProxy parsing library

---
+++ GimmeProxyDotNet/GimmeProxyController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace GimmeProxyDotNet
+{
+	public class GimmeProxyController
+	{
+		public GimmeProxyController()
+		{
+
+		}
+
+		private const string apiBaseUrl = "http://gimmeproxy.com/api/getProxy";
+		private const string baseStr = "/api/getProxy?";
+		private const string messageStr = "country={0}&protocol={1}&get={2}&post={3}&cookies={4}&referer={5}&user-agent={6}&supportsHttps={7}&anonymityLevel={8}&port={9}&maxCheckPeriod={10}&apikey={11}";
+
+
+		private HttpClient GetHttpClient()
+		{
+			var client = new HttpClient();
+			client.BaseAddress = new Uri(apiBaseUrl);
+			Debug.WriteLine("REQUEST HTTP ADDR: " + apiBaseUrl);
+			return client;
+		}
+
+		private async Task<T> ExecuteAsync<T>(string query)
+		{
+			string CallUrl = apiBaseUrl + baseStr + Uri.EscapeDataString(query);
+
+			Debug.WriteLine("REQUEST HTTP ADDR: " + CallUrl);
+
+			using (var client = GetHttpClient())
+			{
+				var json = await client.GetStringAsync(CallUrl);
+				var result = JsonConvert.DeserializeObject<T>(json);
+				return result;
+			}
+		}
+
+		public async Task<GimmeProxyResult> DoQuery(string Country = "", string Protocol = "http", bool AllowGet = true,
+									bool AllowPost = true, bool AllowCookies = true, bool AllowReferer = true,
+									bool AllowUserAgent = true, bool SupportHttps = true, bool AnonymityLevel = true,
+									string Port = "", int MaxCheckPeriod = 86400, string ApiKey = "")
+		{
+			var proxyQuery = string.Format(messageStr, Country, Protocol, 
+			                               AllowGet, AllowPost, AllowCookies, 
+			                               AllowReferer, AllowUserAgent, SupportHttps, 
+			                               AnonymityLevel, Port, MaxCheckPeriod, ApiKey);
+			
+			var result = await this.ExecuteAsync<GimmeProxyResult>(proxyQuery);
+			return result;
+		}
+
+	}
+}
+
---
+++ GimmeProxyDotNet/GimmeProxyResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace GimmeProxyDotNet
+{
+	[JsonObject]
+	public class GimmeProxyResult
+	{
+		[JsonProperty(PropertyName = "get")]
+		public bool AllowGet { get; set; }
+
+		[JsonProperty(PropertyName = "post")]
+		public bool AllowPost { get; set; }
+
+		[JsonProperty(PropertyName = "cookies")]
+		public bool AllowCookies { get; set; }
+
+		[JsonProperty(PropertyName = "referer")]
+		public bool Referer { get; set; }
+
+		[JsonProperty(PropertyName = "user-agent")]
+		public bool UserAgent { get; set; }
+
+		[JsonProperty(PropertyName = "anonymityLevel")]
+		public int AnonymityLevel { get; set; }
+
+		[JsonProperty(PropertyName = "supportsHttps")]
+		public bool SupportsHttps { get; set; }
+
+		[JsonProperty(PropertyName = "protocol")]
+		public string Protocol { get; set; }
+
+		[JsonProperty(PropertyName = "ip")]
+		public string IPAddress { get; set; }
+
+		[JsonProperty(PropertyName = "port")]
+		public string Port { get; set; }
+
+		[JsonProperty(PropertyName = "country")]
+		public string Country { get; set; }
+
+		[JsonProperty(PropertyName = "tsChecked")]
+		public DateTime CheckedTime { get; set; }
+
+		[JsonProperty(PropertyName = "curl")]
+		public string CurlAddr { get; set; }
+
+		[JsonProperty(PropertyName = "ipPort")]
+		public string IpPort { get; set; }
+
+		[JsonProperty(PropertyName = "type")]
+		public string ProxyType { get; set; }
+	}
+}
+
