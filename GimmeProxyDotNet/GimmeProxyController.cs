1723bd81aa0b7e1a34134d97ea1dfa441555b58c
Jackson Ming Hu <huming2207@gmail.com>
2016-08-15 12:06:50 +1000

    Bugfixes

--- GimmeProxyDotNet/GimmeProxyController.cs
+++ GimmeProxyDotNet/GimmeProxyController.cs
@@ -16,7 +16,7 @@ namespace GimmeProxyDotNet
 		private const string apiBaseUrl = "http://gimmeproxy.com";
 		private const string baseStr = "/api/getProxy?";
 		private const string messageStr = "country={0}&protocol={1}&get={2}&post={3}&cookies={4}&referer={5}&user-agent={6}&supportsHttps={7}&anonymityLevel={8}&port={9}&maxCheckPeriod={10}&apikey={11}";
-
+		private const string simplifiedMessageStr = "country={0}&protocol={1}&get={2}&post={3}";
 
 		private HttpClient GetHttpClient()
 		{
@@ -28,7 +28,7 @@ namespace GimmeProxyDotNet
 
 		private async Task<T> ExecuteAsync<T>(string query)
 		{
-			string CallUrl = apiBaseUrl + baseStr + Uri.EscapeDataString(query);
+			string CallUrl = apiBaseUrl + baseStr + query;
 
 			Debug.WriteLine("REQUEST HTTP ADDR: " + CallUrl);
 
@@ -40,7 +40,7 @@ namespace GimmeProxyDotNet
 			}
 		}
 
-		public async Task<GimmeProxyResult> DoQuery(string Country = "", string Protocol = "http", bool AllowGet = true,
+		public async Task<GimmeProxyResult> DoFullQuery(string Country = "", string Protocol = "http", bool AllowGet = true,
 									bool AllowPost = true, bool AllowCookies = true, bool AllowReferer = true,
 									bool AllowUserAgent = true, bool SupportHttps = false, int AnonymityLevel = 0,
 									string Port = "", int MaxCheckPeriod = 86400, string ApiKey = "")
@@ -49,7 +49,18 @@ namespace GimmeProxyDotNet
 			                               AllowGet, AllowPost, AllowCookies, 
 			                               AllowReferer, AllowUserAgent, SupportHttps, 
 			                               AnonymityLevel, Port, MaxCheckPeriod, ApiKey);
+
+			Debug.WriteLine("Query: {0}", proxyQuery);
 			
+			var result = await this.ExecuteAsync<GimmeProxyResult>(proxyQuery);
+			return result;
+		}
+
+
+		public async Task<GimmeProxyResult> DoSimpleQuery(string Country = "", string Protocol = "http", bool AllowGet = true, bool AllowPost = true)
+		{
+			var proxyQuery = string.Format(simplifiedMessageStr, Country, Protocol, AllowGet, AllowPost);
+			Debug.WriteLine("Query: {0}", proxyQuery);
 			var result = await this.ExecuteAsync<GimmeProxyResult>(proxyQuery);
 			return result;
 		}
--- ProxyHandler/Program.cs
+++ ProxyHandler/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Net.NetworkInformation;
 using GimmeProxyDotNet;
 
 namespace ProxyHandler
@@ -8,27 +10,53 @@ namespace ProxyHandler
 	class MainClass
 	{
 		private static GimmeProxyController proxyFetcher = new GimmeProxyController();
+		private static Dictionary<long, string> proxyList = new Dictionary<long, string>();
 
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-			List<string> proxyList = new List<string>();
+
 			Task mainTask = new Task(async () => 
 			{
-				for (int i = 0; i <= 10; i++)
-				{
-					proxyList.Add(await getProxyAddr());
-				}
+				await fetchProxies();
 			});
 			mainTask.Start();
 			mainTask.Wait();
 			Console.ReadLine();
 		}
 
-		public static async Task<string> getProxyAddr()
-		{
-			GimmeProxyResult proxyResult = await proxyFetcher.DoQuery("CN");
-			return proxyResult.IpAndPort;
+
+		public static async Task fetchProxies()
+		{ 
+			
+			for (int i = 0; i <= 20; i++)
+			{
+				Ping pingTester = new Ping();
+				PingOptions pingOptions = new PingOptions(128, true);
+				GimmeProxyResult proxyResult = await proxyFetcher.DoSimpleQuery("CN","http", true, true);
+
+				byte[] pingBuffer = new byte[32];
+
+				PingReply pingReply = pingTester.Send(proxyResult.IpAddress, 500, pingBuffer, pingOptions);
+
+				Console.WriteLine("Retrieved IP: {0}, Latency: {1}", proxyResult.IpAndPort, pingReply.RoundtripTime);
+
+				if (pingReply != null) 
+				{
+					if (!proxyList.ContainsValue(proxyResult.IpAndPort) && !proxyList.ContainsKey(pingReply.RoundtripTime))
+					{
+						proxyList.Add(pingReply.RoundtripTime, proxyResult.IpAndPort);
+					}
+				}
+
+			}
+
+			var sortedProxyList = proxyList.OrderBy(i => i.Key);
+			foreach (var item in sortedProxyList)
+			{
+				long i = item.Key;
+			}
+			Console.WriteLine("Length of Proxy List: {0}", sortedProxyList.Count());
 		}
 	}
 }
